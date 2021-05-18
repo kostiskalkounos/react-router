db1c5134cfd7a6de5e3b9dc49e2b03c95a67a2ce
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-18 11:52:31 +0200

    link buttons to proper pages

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { Route, useParams } from "react-router";
 
+import HighLightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kostis", text: "Neovim > Emacs" },
+  { id: "q2", author: "Mary", text: "I agree" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighLightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
