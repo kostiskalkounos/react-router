5b9ad68588eed436bdb97abc21e69659a09c580d
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-18 11:51:15 +0200

    add input new quote form and dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kostis", text: "Neovim > Emacs" },
+  { id: "q2", author: "Mary", text: "I agree" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
